6e10f19ca4447717e9bd1edb6a8b68a4ba357401
Ankit Agrawal <agrawala96@gmail.com>
2018-12-04 18:27:18 -0500

    iHlba issue fixed

--- Alea/src/xklusac/algorithms/schedule_based/iHlba.java
+++ Alea/src/xklusac/algorithms/schedule_based/iHlba.java
@@ -52,20 +52,22 @@ public class iHlba implements SchedulingPolicy {
         ResourceInfo max_acp_resource = null;
         int max_acp=0;
         for(Map.Entry<ResourceInfo, Integer> entry : res_comPower.entrySet()){
-                if(entry.getValue()>=max_acp){
+                if(entry.getValue()>max_acp){
+                    System.out.println(" max Cp of cluster is " + entry.getValue());
                     max_acp=entry.getValue();
                     max_acp_resource = entry.getKey();
                 }
         }
-        if(max_acp_resource==null)
-            return null;
-        if(canExecuteNowiHlba(gi,max_acp_resource))
-            return max_acp_resource;
-        else{
-            res_comPower.remove(max_acp_resource);
-            getRes(res_comPower,gi);
-        }
-        return null;
+        return max_acp_resource;
+//        if(max_acp_resource==null)
+//            return null;
+//        if(canExecuteNowiHlba(gi,max_acp_resource))
+//            return max_acp_resource;
+//        else{
+//            res_comPower.remove(max_acp_resource);
+//            getRes(res_comPower,gi);
+//        }
+//        return null;
     }
         
     @Override
@@ -82,34 +84,45 @@ public class iHlba implements SchedulingPolicy {
             for (int j = 0; j < Scheduler.resourceInfoList.size(); j++) {
               // calculate average computing power of each resource
               // and select the one with highest computing power
-              ResourceInfo ri = (ResourceInfo)Scheduler.resourceInfoList.get(j);
+              
+              
+              for(int m = 0; m < Scheduler.resourceInfoList.size(); m++){
+                  ResourceInfo ri = (ResourceInfo)Scheduler.resourceInfoList.get(m);
               int acp = ri.calculate_avg_computingPower();
+              
               System.out.println("avg_computingPower is "+acp);
                // find the resource with max computing power
               //res_comPower.put(Integer.toString(ri.resource.getResourceID()),Integer.toString(acp));
               res_comPower.put(ri,acp);
+              }
+              
 //              if(acp>max_acp){
 //                max_acp=acp;
 //                max_acp_resource=ri;
 //              } 
-            }
-//            for(Map.Entry<ResourceInfo, Integer> entry : res_comPower.entrySet()){
-//                if(entry.getValue()>max_acp){
-//                    max_acp_resource = entry.getKey();
-//                }
-//            }
             
             max_acp_resource= getRes(res_comPower,gi);
+//            if(max_acp_resource==null)
+//                System.out.println("MAx Acp resource is null");
+//            else{
+//                for (int k = 0; k < Scheduler.resourceInfoList.size(); k++) {
+//                if (Scheduler.isSuitable(max_acp_resource, gi) && max_acp_resource.canExecuteNow(gi)) {
+//
+//                    r_cand = max_acp_resource;
+//                    break;
+//                }
+//            }
+//            }
             
-            
+             
             //&& Scheduler.isSuitable(max_acp_resource, gi) && max_acp_resource.canExecuteNow(gi)
-            if (max_acp_resource != null) {
+            if (max_acp_resource != null && max_acp_resource.canExecuteNow(gi)) {
                 gi = (GridletInfo) Scheduler.queue.remove(i);
                 //System.err.println(gi.getID()+" PEs size = "+gi.PEs.size());
                 max_acp_resource.addGInfoInExec(gi);
                 // set the resource ID for this gridletInfo (this is the final scheduling decision)
                 gi.setResourceID(max_acp_resource.resource.getResourceID());
-                scheduler.submitJobHlba(gi.getGridlet(), max_acp_resource.resource.getResourceID());
+                scheduler.submitJob(gi.getGridlet(), max_acp_resource.resource.getResourceID());
                 max_acp_resource.is_ready = true;
                 //scheduler.sim_schedule(GridSim.getEntityId("Alea_3.0_scheduler"), 0.0, AleaSimTags.GRIDLET_SENT, gi);
                 scheduled++;
@@ -120,7 +133,7 @@ public class iHlba implements SchedulingPolicy {
                 return scheduled;
             }
         }
-            
+    }
          return scheduled;
         }
 
